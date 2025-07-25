21c6ac011d6f21af60f8f8894a627f6beedd9c96
Dev-Rohiit <KUSHWAHAROHIT908@GMAIL.COM>
2025-07-25 16:14:08 +0530

    Add files via upload

---
+++ Lecture 12/index.js
@@ -0,0 +1,35 @@
+// function solve(){
+//     var x = 10;
+//     console.log(x)
+// }
+// solve()
+// console.log(x)
+
+// var x = 10;
+// var x = 20;
+// console.log(x);
+
+// let x = 20;
+// console.log(x);
+// let y  =30;
+// console.log(y);
+
+// const x = 10;
+// console.log(x)
+// x = 20;
+// console.log(x);
+
+let x = 20;
+x = 30.346;
+console.log(typeof(x)); //number
+
+var z = "Rohit";
+console.log(typeof(z)); //string
+
+var a = true;
+console.log(typeof(a)); //Boolean
+
+var b;
+console.log(typeof(b)); //undefine
+
+
